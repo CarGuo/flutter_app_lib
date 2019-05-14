6d9d370efbe54407f6d95ce7c5e827b0f88dccd4
guoshuyu <359369982@qq.com>
2019-05-14 17:16:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+### Flutter Lib
+
+这是一个测试将 Flutter 工程打包成 aar 的项目，单纯支持 Android，测试混合开发结合的可行性。
+
+
+flutter packages get 之后，执行 flutter clean，之后在android目录下执行 ./gradlew assembleRelease
---
+++ android/app/src/main/kotlin/com/shuyu/flutter_app_lib/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shuyu.flutter_app_lib
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
